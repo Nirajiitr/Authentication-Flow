def486d43dc0663e7f5a700f4896a1f5700db005
Niraj Kumar <nk659192@gmail.com>
2024-09-28 15:31:13 +0530

    complete navbar

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const hamburger = document.querySelector(".hamburger");
+const menuList = document.querySelector(".menu-list");
+const dropdown = document.querySelector("#dropdown");
+const subList = document.querySelector(".sub-list");
+
+
+function handleDropdown() {
+  if (window.innerWidth >= 1024) {
+  
+    dropdown.addEventListener("mouseenter", () => {
+      subList.style.display = "block";
+    });
+
+    dropdown.addEventListener("mouseleave", () => {
+      subList.style.display = "none";
+    });
+  } else {
+    subList.style.display = "block"; 
+  }
+}
+
+handleDropdown();
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menuList.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((i) =>
+  i.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menuList.classList.remove("active");
+  })
+);
+
